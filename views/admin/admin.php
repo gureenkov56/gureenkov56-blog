206314ca2312966dad769b60b8f8bde98573e46b
Никита Гуреенков <nikitagureenkov@MacBook-Air-Nikita.local>
2022-03-15 16:03:03 +0300

    Clear by VUE

--- views/admin/admin.php
+++ views/admin/admin.php
@@ -10,22 +10,10 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/php/functions.php';
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ADMIN PANEL</title>
-    <script src="https://unpkg.com/vue@3"></script>
     <link href="../../styles/admin-style.css" rel="stylesheet">
-
-    <script type="importmap">
-  {
-    "imports": {
-      "vue": "https://unpkg.com/vue@3/dist/vue.esm-browser.js"
-    }
-  }
-</script>
-
 </head>
 <body>
     <div id="app">
-        {{ message }}
-        <mycomponent></mycomponent>
         <div class="direction__wrapper">
             <div class="direction-item">
                 <img src="../img/admin/dir-icon.png" alt="Direction">
@@ -88,9 +76,6 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/php/functions.php';
             </div>
         </div>
     </div>
-
-    <script src="/views/admin/main.js" type='module'></script>
-
 
 </body>
 </html>
--- views/admin/my-component.js
+++
@@ -1,9 +0,0 @@
-// my-component.js
-export default {
-  name: 'MyComponent',
-  data() {
-    return { count: 0 }
-  },
-  template: `<div>count is {{ count }}</div>`
-}
-
