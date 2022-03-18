0f2e14629d8195fe89094ed77dbb6b03e6f05a32
Никита Гуреенков <nikitagureenkov@MacBook-Air-Nikita.local>
2022-03-18 17:39:07 +0300

    Add 5 the latest post oi folder - need in bettering

--- js/admin.js
+++ js/admin.js
@@ -1,106 +1,112 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const app = document.getElementById('app'),
-        startMenu = document.getElementById('startMenu'),
-        startBtn = document.getElementById('startBtn'),
-        directionItem = document.querySelectorAll('.direction-item'),
-        windowsRedBtn = document.querySelectorAll('.windows_red_btn');
+document.addEventListener("DOMContentLoaded", () => {
+  const app = document.getElementById("app"),
+    startMenu = document.getElementById("startMenu"),
+    startBtn = document.getElementById("startBtn"),
+    directionItem = document.querySelectorAll(".direction-item"),
+    windowsRedBtn = document.querySelectorAll(".windows_red_btn");
 
-    let lastZIndex = 1;
+  let lastZIndex = 1;
 
-    // listener for click out of some blocks
-    document.addEventListener('click', (event) => {
-        const clickedElement = event.composedPath();
+  // listener for click out of some blocks
+  document.addEventListener("click", (event) => {
+    const clickedElement = event.composedPath();
 
-        if (!clickedElement.includes(startMenu) && !clickedElement.includes((startBtn))) {
-            startMenu.classList.remove('show');
-        }
-    })
-
-    // show and hide start menu
-    startBtn.addEventListener('click', () => {
-        startMenu.style.zIndex = lastZIndex;
-        startMenu.classList.toggle('show');
-        lastZIndex++;
-    })
-
-    windowsRedBtn.forEach(redBtn => {
-        redBtn.addEventListener('click', () => {
-            redBtn.parentElement.parentElement.classList.remove('show');
-        })
-    })
-
-    // click for open folder
-    directionItem.forEach(folder => {
-        folder.addEventListener('click', () => {
-            const ID = folder.dataset.link;
-
-            // if folder was open
-            const openBeforeWindow = document.getElementById(ID);
-            if (openBeforeWindow) {
-                openBeforeWindow.style.zIndex = lastZIndex;
-                lastZIndex++;
-                return;
-            }
-
-            const newWindow = document.querySelector('#folderTemplate').cloneNode(true);
-
-            newWindow.querySelector('#windowName').innerHTML = ID;
-            newWindow.id = ID;
-            newWindow.classList.remove('hide');
-            newWindow.style.zIndex = lastZIndex;
-
-            // random coordinates
-            let mt = Math.random() * 30 + 'px',
-                ml = Math.random() * 30 + '%';
-
-            newWindow.style.marginTop = mt;
-            newWindow.style.marginLeft = ml;
-
-
-            app.appendChild(newWindow);
-            lastZIndex++;
-            refreshBtnOnScreen();
-
-            if (ID === 'Posts') {
-                let windowsFolderBodyRightWrapper = newWindow.querySelector('.windows__folder__body__right__wrapper');
-                let contentItemTemplate = newWindow.querySelector('.windows__folder__body__right__file');
-
-                windowsFolderBodyRightWrapper.innerHTML = '';
-
-                fetch(`${window.location.origin}/api/posts?` + new URLSearchParams({
-                    select: '*',
-                    from: 'posts',
-                    orderBy: 'pub_date',
-                    order: 'DESC',
-                    limit: '5',
-                })
-                )
-                    .then(res => res.json())
-                    .then(data => {
-                        data.forEach(contentItem => {
-                            let newContentDOM = contentItemTemplate.cloneNode(true);
-                            newContentDOM.querySelector('.fileinfo__title').innerHTML = contentItem.h1;
-                            newContentDOM.querySelector('.fileinfo__status_pub').innerHTML = contentItem.pub_date;
-
-
-                            windowsFolderBodyRightWrapper.appendChild(newContentDOM);
-                        })
-                    })
-
-            }
-        })
-    })
-
-    function refreshBtnOnScreen() {
-        const closeWindowBtn = document.querySelectorAll('.windows_red_btn');
-
-        closeWindowBtn.forEach(redBtn => {
-            redBtn.addEventListener('click', () => {
-                redBtn.closest('.folder').remove();
-            })
-        })
+    if (
+      !clickedElement.includes(startMenu) &&
+      !clickedElement.includes(startBtn)
+    ) {
+      startMenu.classList.remove("show");
     }
-})
+  });
 
+  // show and hide start menu
+  startBtn.addEventListener("click", () => {
+    startMenu.style.zIndex = lastZIndex;
+    startMenu.classList.toggle("show");
+    lastZIndex++;
+  });
 
+  windowsRedBtn.forEach((redBtn) => {
+    redBtn.addEventListener("click", () => {
+      redBtn.parentElement.parentElement.classList.remove("show");
+    });
+  });
 
+  // click for open folder
+  directionItem.forEach((folder) => {
+    folder.addEventListener("click", () => {
+      const ID = folder.dataset.link;
+
+      // if folder was open
+      const openBeforeWindow = document.getElementById(ID);
+      if (openBeforeWindow) {
+        openBeforeWindow.style.zIndex = lastZIndex;
+        lastZIndex++;
+        return;
+      }
+
+      const newWindow = document
+        .querySelector("#folderTemplate")
+        .cloneNode(true);
+
+      newWindow.querySelector("#windowName").innerHTML = ID;
+      newWindow.id = ID;
+      newWindow.classList.remove("hide");
+      newWindow.style.zIndex = lastZIndex;
+
+      // random coordinates
+      let mt = Math.random() * 30 + "px",
+        ml = Math.random() * 30 + "%";
+
+      newWindow.style.marginTop = mt;
+      newWindow.style.marginLeft = ml;
+
+      app.appendChild(newWindow);
+      lastZIndex++;
+      refreshBtnOnScreen();
+
+      if (ID === "Posts") {
+        let windowsFolderBodyRightWrapper = newWindow.querySelector(
+          ".windows__folder__body__right__wrapper"
+        );
+        let contentItemTemplate = newWindow.querySelector(
+          ".windows__folder__body__right__file"
+        );
+
+        windowsFolderBodyRightWrapper.innerHTML = "";
+
+        fetch(
+          `${window.location.origin}/api/posts?` +
+            new URLSearchParams({
+              select: "id, h1, pub_date, views",
+              from: "posts",
+              orderBy: "pub_date",
+              order: "DESC",
+              limit: "5",
+            })
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            data.forEach((contentItem) => {
+              let newContentDOM = contentItemTemplate.cloneNode(true);
+              newContentDOM.querySelector(".fileinfo__title").innerHTML = contentItem.h1.slice(0, 17) + '...';
+              newContentDOM.querySelector(".fileinfo__status_pub").innerHTML = contentItem.pub_date;
+              newContentDOM.querySelector(".views_count").innerHTML = contentItem.views;
+
+              windowsFolderBodyRightWrapper.appendChild(newContentDOM);
+            });
+          });
+      }
+    });
+  });
+
+  function refreshBtnOnScreen() {
+    const closeWindowBtn = document.querySelectorAll(".windows_red_btn");
+
+    closeWindowBtn.forEach((redBtn) => {
+      redBtn.addEventListener("click", () => {
+        redBtn.closest(".folder").remove();
+      });
+    });
+  }
+});
--- views/admin/folder.php
+++ views/admin/folder.php
@@ -3,7 +3,7 @@
     <div class="windows__header">
         <span class="windows__header__name">
             <img src="../../img/admin/folder-icon.png" alt="folder-icon">
-            <span id="windowName">Posts</span>
+        <span id="windowName">Posts</span>
         </span>
         <div class="windows__header__btn_wrapper">
             <button class="windows__header__btn windows_blue_btn hover_light active_darker">_</button>
@@ -29,178 +29,37 @@
                     </ul>
                 </div>
             </div>
+
+            <div class="windows__folder__body__left__menuWrapper">
+                <div class="windows__folder__body__left__menuWrapper__header">
+                    <span>Директории</span>
+                    <img src="../../img/admin/folder-leftmenu-icon.png" alt="hider-icon">
+                </div>
+                <div class="windows__folder__body__left__menuWrapper__body">
+                    <ul>
+                        <li>Категории</li>
+                        <li>Не опубликованные</li>
+                        <li>Корзина</li>
+                    </ul>
+                </div>
+            </div>
+
         </div>
         <div class="windows__folder__body__right">
+            <div class="windows__folder__body__right__wrapper__counter">
+                Показано <span id="postsCountVuew">5</span> из <span id="postsCountTotal">??</span> файлов
+            </div>
 
             <div class="windows__folder__body__right__wrapper">
-                <div class="windows__folder__body__right__file">
-                    <img src="../../img/admin/textfile-icon.png" alt="text-file">
-                    <div class="fileinfo__wrapper">
-                        <div class="fileinfo__title">Первый тестовый пост.txt</div>
-                        <div class="fileinfo__status_pub">25.05.2008</div>
-                        <div class="fileinfo__views">Просмотры: 566</div>
-                    </div>
-                </div>
+                
 
+                <!--post template-->
                 <div class="windows__folder__body__right__file">
                     <img src="../../img/admin/textfile-icon.png" alt="text-file">
-                    <div class="fileinfo__wrapper">
-                        <div class="fileinfo__title">Первый тестовый пост.txt</div>
-                        <div class="fileinfo__status_pub">25.05.2008</div>
-                        <div class="fileinfo__views">Просмотры: 566</div>
-                    </div>
-                </div>
-
-                <div class="windows__folder__body__right__file">
-                    <img src="../../img/admin/textfile-icon.png" alt="text-file">
-                    <div class="fileinfo__wrapper">
-                        <div class="fileinfo__title">Первый тестовый пост.txt</div>
-                        <div class="fileinfo__status_pub">25.05.2008</div>
-                        <div class="fileinfo__views">Просмотры: 566</div>
-                    </div>
-                </div>
-
-                <div class="windows__folder__body__right__file">
-                    <img src="../../img/admin/textfile-icon.png" alt="text-file">
-                    <div class="fileinfo__wrapper">
-                        <div class="fileinfo__title">Первый тестовый пост.txt</div>
-                        <div class="fileinfo__status_pub">25.05.2008</div>
-                        <div class="fileinfo__views">Просмотры: 566</div>
-                    </div>
-                </div>
-
-                <div class="windows__folder__body__right__file">
-                    <img src="../../img/admin/textfile-icon.png" alt="text-file">
-                    <div class="fileinfo__wrapper">
-                        <div class="fileinfo__title">Первый тестовый пост.txt</div>
-                        <div class="fileinfo__status_pub">25.05.2008</div>
-                        <div class="fileinfo__views">Просмотры: 566</div>
-                    </div>
-                </div>
-
-                <div class="windows__folder__body__right__file">
-                    <img src="../../img/admin/textfile-icon.png" alt="text-file">
-                    <div class="fileinfo__wrapper">
-                        <div class="fileinfo__title">Первый тестовый пост.txt</div>
-                        <div class="fileinfo__status_pub">25.05.2008</div>
-                        <div class="fileinfo__views">Просмотры: 566</div>
-                    </div>
-                </div>
-
-                <div class="windows__folder__body__right__file">
-                    <img src="../../img/admin/textfile-icon.png" alt="text-file">
-                    <div class="fileinfo__wrapper">
-                        <div class="fileinfo__title">Первый тестовый пост.txt</div>
-                        <div class="fileinfo__status_pub">25.05.2008</div>
-                        <div class="fileinfo__views">Просмотры: 566</div>
-                    </div>
-                </div>
-
-                <div class="windows__folder__body__right__file">
-                    <img src="../../img/admin/textfile-icon.png" alt="text-file">
-                    <div class="fileinfo__wrapper">
-                        <div class="fileinfo__title">Первый тестовый пост.txt</div>
-                        <div class="fileinfo__status_pub">25.05.2008</div>
-                        <div class="fileinfo__views">Просмотры: 566</div>
-                    </div>
-                </div>
-
-                <div class="windows__folder__body__right__file">
-                    <img src="../../img/admin/textfile-icon.png" alt="text-file">
-                    <div class="fileinfo__wrapper">
-                        <div class="fileinfo__title">Первый тестовый пост.txt</div>
-                        <div class="fileinfo__status_pub">25.05.2008</div>
-                        <div class="fileinfo__views">Просмотры: 566</div>
-                    </div>
-                </div>
-
-                <div class="windows__folder__body__right__file">
-                    <img src="../../img/admin/textfile-icon.png" alt="text-file">
-                    <div class="fileinfo__wrapper">
-                        <div class="fileinfo__title">Первый тестовый пост.txt</div>
-                        <div class="fileinfo__status_pub">25.05.2008</div>
-                        <div class="fileinfo__views">Просмотры: 566</div>
-                    </div>
-                </div>
-
-                <div class="windows__folder__body__right__file">
-                    <img src="../../img/admin/textfile-icon.png" alt="text-file">
-                    <div class="fileinfo__wrapper">
-                        <div class="fileinfo__title">Первый тестовый пост.txt</div>
-                        <div class="fileinfo__status_pub">25.05.2008</div>
-                        <div class="fileinfo__views">Просмотры: 566</div>
-                    </div>
-                </div>
-
-                <div class="windows__folder__body__right__file">
-                    <img src="../../img/admin/textfile-icon.png" alt="text-file">
-                    <div class="fileinfo__wrapper">
-                        <div class="fileinfo__title">Первый тестовый пост.txt</div>
-                        <div class="fileinfo__status_pub">25.05.2008</div>
-                        <div class="fileinfo__views">Просмотры: 566</div>
-                    </div>
-                </div>
-
-                <div class="windows__folder__body__right__file">
-                    <img src="../../img/admin/textfile-icon.png" alt="text-file">
-                    <div class="fileinfo__wrapper">
-                        <div class="fileinfo__title">Первый тестовый пост.txt</div>
-                        <div class="fileinfo__status_pub">25.05.2008</div>
-                        <div class="fileinfo__views">Просмотры: 566</div>
-                    </div>
-                </div>
-
-                <div class="windows__folder__body__right__file">
-                    <img src="../../img/admin/textfile-icon.png" alt="text-file">
-                    <div class="fileinfo__wrapper">
-                        <div class="fileinfo__title">Первый тестовый пост.txt</div>
-                        <div class="fileinfo__status_pub">25.05.2008</div>
-                        <div class="fileinfo__views">Просмотры: 566</div>
-                    </div>
-                </div>
-
-                <div class="windows__folder__body__right__file">
-                    <img src="../../img/admin/textfile-icon.png" alt="text-file">
-                    <div class="fileinfo__wrapper">
-                        <div class="fileinfo__title">Первый тестовый пост.txt</div>
-                        <div class="fileinfo__status_pub">25.05.2008</div>
-                        <div class="fileinfo__views">Просмотры: 566</div>
-                    </div>
-                </div>
-
-                <div class="windows__folder__body__right__file">
-                    <img src="../../img/admin/textfile-icon.png" alt="text-file">
-                    <div class="fileinfo__wrapper">
-                        <div class="fileinfo__title">Первый тестовый пост.txt</div>
-                        <div class="fileinfo__status_pub">25.05.2008</div>
-                        <div class="fileinfo__views">Просмотры: 566</div>
-                    </div>
-                </div>
-
-                <div class="windows__folder__body__right__file">
-                    <img src="../../img/admin/textfile-icon.png" alt="text-file">
-                    <div class="fileinfo__wrapper">
-                        <div class="fileinfo__title">Первый тестовый пост.txt</div>
-                        <div class="fileinfo__status_pub">25.05.2008</div>
-                        <div class="fileinfo__views">Просмотры: 566</div>
-                    </div>
-                </div>
-
-                <div class="windows__folder__body__right__file">
-                    <img src="../../img/admin/textfile-icon.png" alt="text-file">
-                    <div class="fileinfo__wrapper">
-                        <div class="fileinfo__title">Первый тестовый пост.txt</div>
-                        <div class="fileinfo__status_pub">25.05.2008</div>
-                        <div class="fileinfo__views">Просмотры: 566</div>
-                    </div>
-                </div>
-
-                <div class="windows__folder__body__right__file">
-                    <img src="../../img/admin/textfile-icon.png" alt="text-file">
-                    <div class="fileinfo__wrapper">
-                        <div class="fileinfo__title">Первый тестовый пост.txt</div>
-                        <div class="fileinfo__status_pub">25.05.2008</div>
-                        <div class="fileinfo__views">Просмотры: 566</div>
+                    <div class="fileinfo__wrapper" title="TEST TITLE">
+                        <div class="fileinfo__title"></div>
+                        <div class="fileinfo__status_pub"></div>
+                        <div class="fileinfo__views">Просмотры: <span class="views_count"></span></div>
                     </div>
                 </div>
 
