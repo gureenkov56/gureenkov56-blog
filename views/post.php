939594b221ae931591e7bc985c04a7c71f84d9bf
nikitagureenkov <gureenkov56@gmail.com>
2023-01-09 10:45:59 +0400

    Last update

--- views/post.php
+++ views/post.php
@@ -1,95 +1,73 @@
 <?php
 
-  require $_SERVER['DOCUMENT_ROOT'] . "/php/functions.php";
+    require $_SERVER['DOCUMENT_ROOT'] . "/php/functions.php";
 
-  $res = $pdo->prepare("SELECT * FROM `posts` WHERE `id` = ? LIMIT 1");
-  $res->execute([$_GET['id']]);
-  $post = $res->fetch(PDO::FETCH_ASSOC);
+    $res = $pdo->prepare("SELECT * FROM `posts` WHERE `id` = ? LIMIT 1");
+    $res->execute([$_GET['id']]);
+    $post = $res->fetch(PDO::FETCH_ASSOC);
 
-  if (empty($post)) header("Location: /404");
+    if (empty($post)) header("Location: /404");
 
-// filter fragments
+    // filter fragments
 
-  $post_content = $post['text'];
+    $post_content = $post['text'];
 
-  if (empty($_SESSION)) {
-    $regular_for_clean = '/==access_level=1 start==.*.==access_level=1 end==|==access_level=2 start==.*.==access_level=2 end==|==access_level=3 start==.*.==access_level=3 end==/';
-  } elseif ($_SESSION['access_level'] === '1') {
-    $regular_for_clean = '/==access_level=2 start==.*.==access_level=2 end==|==access_level=3 start==.*.==access_level=3 end==/';
-    $regular_for_open_span = '/==access_level=1 start==/';
-    $regular_for_close_span = '/==access_level=1 end==/';
-  } elseif ($_SESSION['access_level'] === '2') {
-    $regular_for_clean = '/==access_level=3 start==.*.==access_level=3 end==/';
-    $regular_for_open_span = '/==access_level=1 start==|==access_level=2 start==/';
-    $regular_for_close_span = '/==access_level=1 end==|==access_level=2 end==/';
-  } elseif ($_SESSION['access_level'] === 'admin' || $_SESSION['access_level'] === '3') {
-    $regular_for_open_span = '/==access_level=1 start==|==access_level=2 start==|==access_level=3 start==/';
-    $regular_for_close_span = '/==access_level=1 end==|==access_level=2 end==|==access_level=3 end==/';
-  }
+    if (empty($_SESSION)) {
+        $regular_for_clean = '/==access_level=1 start==.*.==access_level=1 end==|==access_level=2 start==.*.==access_level=2 end==|==access_level=3 start==.*.==access_level=3 end==/';
+    } elseif ($_SESSION['access_level'] === '1') {
+        $regular_for_clean = '/==access_level=2 start==.*.==access_level=2 end==|==access_level=3 start==.*.==access_level=3 end==/';
+        $regular_for_open_span = '/==access_level=1 start==/';
+        $regular_for_close_span = '/==access_level=1 end==/';
+    } elseif ($_SESSION['access_level'] === '2') {
+        $regular_for_clean = '/==access_level=3 start==.*.==access_level=3 end==/';
+        $regular_for_open_span = '/==access_level=1 start==|==access_level=2 start==/';
+        $regular_for_close_span = '/==access_level=1 end==|==access_level=2 end==/';
+    } elseif ($_SESSION['access_level'] === 'admin' || $_SESSION['access_level'] === '3') {
+        $regular_for_open_span = '/==access_level=1 start==|==access_level=2 start==|==access_level=3 start==/';
+        $regular_for_close_span = '/==access_level=1 end==|==access_level=2 end==|==access_level=3 end==/';
+    }
 
-  if (isset($regular_for_clean)) {
-    $post_content = preg_replace($regular_for_clean, "", $post_content);
-  }
+    if (isset($regular_for_clean)) {
+        $post_content = preg_replace($regular_for_clean, "", $post_content);
+    }
 
-  if (isset($regular_for_open_span) && isset($regular_for_close_span)) {
-    $post_content = preg_replace($regular_for_open_span, "<span class='fragment'>", $post_content);
-    $post_content = preg_replace($regular_for_close_span, "</span>", $post_content);
-  }
+    if (isset($regular_for_open_span) && isset($regular_for_close_span)) {
+        $post_content = preg_replace($regular_for_open_span, "<span class='fragment'>", $post_content);
+        $post_content = preg_replace($regular_for_close_span, "</span>", $post_content);
+    }
 
 
-// views +1 counter
-  pdo_update('posts', ['views' => $post['views'] + 1], ['id' => $post['id']]);
+    // views +1 counter
+    pdo_update('posts', ['views' => $post['views'] + 1], ['id' => $post['id']]);
 
-  include_once "modules/header-post.php";
+    include_once "modules/header-post.php";
 ?>
 
-    <article class="post">
-        <div class="post__titleBlock">
-            <h1><?= $post['h1'] ?></h1>
-            <img src="https://gureenkov56.ru/img/post/<?= $post['preview_img'] ?>" alt="" class="post__mainImage">
+<article class="post">
+    <div class="post__titleBlock">
+        <h1><?= $post['h1'] ?></h1>
+        <img src="https://gureenkov56.ru/img/post/<?= $post['preview_img'] ?>" alt="" class="post__mainImage">
+    </div>
+    <?php if ($post['level_access'] > 0) : ?>
+        <div class="post__access-msg">
+            <input type="text" id="inputForKaomoji" hidden>
+            <span>⭐</span>Пост для своих<span>⭐</span>
+            <p id="japanKaomoji">( ಠ ͜ʖ ಠ)</p>
+
+            <p id="copyKaomoji">Скопировать kaomoji</p>
         </div>
-      <?php if ($post['level_access'] > 0) : ?>
-          <div class="post__access-msg">
-              <input type="text" id="inputForKaomoji" hidden>
-              <span>⭐</span>Пост для своих<span>⭐</span>
-              <p id="japanKaomoji">( ಠ ͜ʖ ಠ)</p>
+    <?php endif; ?>
+    <?= $post_content ?>
 
-              <p id="copyKaomoji">Скопировать kaomoji</p>
-          </div>
-      <?php endif; ?>
-      <?= $post_content ?>
-
-<!--        <div class="js-story story__wrapper"-->
-<!--             data-images="https://s8.stc.all.kpcdn.net/russia/wp-content/uploads/2019/01/Altai-.jpg|https://chudo-udo.info/images/stories/kontent2/167_19.jpg|https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Moench_2339.jpg/1200px-Moench_2339.jpg"-->
-<!--             data-descriptions="desc 1|desc2| desc 3">-->
-<!---->
-<!--            <div class="js-story-img-container story_full-height">-->
-<!--                <p>Загрузка...</p>-->
-<!--            </div>-->
-<!--            <div class="story__counter">-->
-<!---->
-<!--            </div>-->
-<!--            <div class="story__togglers">-->
-<!--                <div class="back">-->
-<!--                    <img src="../img/svg/arrow-left.svg" alt="back">-->
-<!--                </div>-->
-<!--                <div class="middle"></div>-->
-<!--                <div class="next">-->
-<!--                    <img src="../img/svg/arrow-left.svg" alt="back">-->
-<!--                </div>-->
-<!--            </div>-->
-<!---->
-<!--        </div>-->
-
-    </article>
+</article>
 
 <?php include_once "modules/reactions.php" ?>
 
-    <script>
-      const post_id = <?= $_GET['id'] ?>;
-    </script>
-    <script src="/js/post.js"></script>
+<script>
+  const post_id = <?= $_GET['id'] ?>;
+</script>
+<script src="/js/post.js"></script>
 
 <?php
-  include_once "modules/footer.php"
+    include_once "modules/footer.php"
 ?>
