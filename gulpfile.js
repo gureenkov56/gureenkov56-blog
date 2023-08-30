92fc8b5c915b2e09f830944ea2c3d564b652f93e
gureenkov56 <gureenkov56@gmail.com>
2023-08-30 22:07:04 +0500

    Gulp for styles and scripts

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+import gulp from 'gulp';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import ts from 'gulp-typescript';
+import uglify from 'gulp-uglify'
+import clean from 'gulp-clean';
+
+const sass = gulpSass(dartSass);
+const { src, dest, series, watch, task } = gulp;
+
+
+function js() {
+    return src('src/ts/*.ts')
+        .pipe(ts({
+            noImplicitAny: true,
+
+            outDir: 'frontend/js/',
+            target: 'ES5'
+        }))
+        .pipe(uglify())
+        .pipe(dest('frontend/js'))
+}
+
+function css() {
+    return src('src/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest('frontend/css/'))
+}
+function startwatch() {
+    watch('src/ts/*.js', js)
+    watch('src/scss/*.scss', css)
+}
+
+async function cleanFolder() {
+    return src('frontend/*')
+        .pipe(clean());
+}
+
+// gulp
+task('default', startwatch);
+
+// gulp styles
+task('styles', css)
+
+// gulp js
+task('js', js)
+
+// gulp build
+task('build', series(cleanFolder, js, css))
+
---
+++ src/ts/test.ts
@@ -0,0 +1,11 @@
+type Test = {
+    key: string
+    value: boolean
+}
+
+const obj: Test = {
+    key: 'isTest',
+    value: true
+}
+
+console.log(obj)
