309d7677957ca5985d98196de58bfafe1db17fcb
gureenkov56 <gureenkov56@gmail.com>
2023-11-25 01:32:04 +0500

    404 with fun gifs

--- backend/views/404_view.php
+++ backend/views/404_view.php
@@ -1,1 +1,21 @@
-<h1>404</h1>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="/frontend/css/404.css">
+    <title>Упс... Что-то пошло не так</title>
+</head>
+<body>
+<div class="page-404">
+    <div>
+        <img src="/frontend/public/images/gif/404/<?=rand(1, 8);?>.gif" alt="404-gif" width="300px">
+        <h1>404</h1>
+        <div class="text">Упс... Что-то пошло не так</div>
+        <a href="/">Вернуться домой</a>
+    </div>
+</div>
+</body>
+</html>
+
+
