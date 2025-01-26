72b81c543239ffeb2001f63205f0219275a57b17
Nikita Gureenkov <gureenkov.n@promail366.com>
2025-01-26 22:49:13 +0500

    Yandex Metrika was added

--- backend/views/template_view.php
+++ backend/views/template_view.php
@@ -11,12 +11,17 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="/frontend/css/main.css">
-    <meta name="description" content="<?= (key_exists('SEO_description', $data)) ? $data['SEO_description'] : $GLOBALS['BLOG_DESCRIPTION']; ?>"/>
-    <title><?= (key_exists('SEO_title', $data)) ? $data['SEO_title'] : $GLOBALS['DEFAULT_SEO_TITLE']; ?></title>
+    <meta name="description" content="<?= key_exists("SEO_description", $data)
+        ? $data["SEO_description"]
+        : $GLOBALS["BLOG_DESCRIPTION"] ?>"/>
+    <title><?= key_exists("SEO_title", $data)
+        ? $data["SEO_title"]
+        : $GLOBALS["DEFAULT_SEO_TITLE"] ?></title>
 </head>
 <body>
-<?php require_once 'parts/header/header.php'; ?>
-<?php require_once 'parts/middle.php' ?>
-<?php require_once 'parts/footer.php' ?>
+<?php require_once "parts/header/metrika.php"; ?>
+<?php require_once "parts/header/header.php"; ?>
+<?php require_once "parts/middle.php"; ?>
+<?php require_once "parts/footer.php"; ?>
 </body>
 </html>
